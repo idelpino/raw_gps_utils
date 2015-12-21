037f4e7fd17e8a288cc412907958d137369e35ff
Pier Tirindelli <pier.tirindelli@gmail.com>
2015-12-21 10:46:10 +0100

    moved obsData implementation in .cpp

--- src/obs_data.cpp
+++ src/obs_data.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "obs_data.h"
+
+ObsData::ObsData(const std::string &_sat_id, const rawgpsutils::ScalarT &_timestamp, rawgpsutils::ScalarT &_pseudorange)
+        : sat_id_(_sat_id), timestamp_(_timestamp), pseudorange_(_pseudorange)
+{
+    //std::cout << "ObsData constructor: " << toString();
+}
+
+void ObsData::calculateSatPosition()
+{
+    sat_position_ = Eigen::Vector3s(50000+pseudorange_, 60000+pseudorange_, 70000+pseudorange_);
+}
+
+std::string ObsData::toString()
+{
+    std::ostringstream s;
+    s << "SatID: " << sat_id_ << " -- " << /*timestamp_.get()*/timestamp_ << " -- " << pseudorange_ << " -- (" << sat_position_[0] << ", " << sat_position_[1] << ", " << sat_position_[2];
+    return s.str();
+}
+
+const std::string &ObsData::getSatId() const
+{
+    return sat_id_;
+}
+
+const rawgpsutils::ScalarT &ObsData::getTimestamp() const
+{
+    return timestamp_;
+}
+
+rawgpsutils::ScalarT ObsData::getPseudorange() const
+{
+    return pseudorange_;
+}
+
+Eigen::Vector3s ObsData::getSatPosition() const
+{
+    return sat_position_;
+}
+
+void ObsData::setSatPosition(const Eigen::Vector3s &sat_position_)
+{
+    ObsData::sat_position_ = sat_position_;
+}
--- src/obs_data.h
+++ src/obs_data.h
@@ -12,52 +12,21 @@
 class ObsData
 {
 public:
-    ObsData(const std::string &_sat_id, const rawgpsutils::ScalarT &_timestamp, rawgpsutils::ScalarT &_pseudorange) :
-            sat_id_(_sat_id), timestamp_(_timestamp), pseudorange_(_pseudorange)
-    {
-        //std::cout << "ObsData constructor: " << toString();
-    }
+    ObsData(const std::string &_sat_id, const rawgpsutils::ScalarT &_timestamp, rawgpsutils::ScalarT &_pseudorange);
 
-    /** TODO now it's a dummy function
-     * Function to calculate the sat position in the time timestamp_
-     */
-    void calculateSatPosition()
-    {
-        sat_position_ = Eigen::Vector3s(50000+pseudorange_, 60000+pseudorange_, 70000+pseudorange_);
-    }
+    void calculateSatPosition();
 
-    std::string toString()
-    {
-        std::ostringstream s;
-        s << "SatID: " << sat_id_ << " -- " << /*timestamp_.get()*/timestamp_ << " -- " << pseudorange_ << " -- (" << sat_position_[0] << ", " << sat_position_[1] << ", " << sat_position_[2];
-        return s.str();
-    }
+    std::string toString();
 
-    const std::string &getSatId() const
-    {
-        return sat_id_;
-    }
+    const std::string & getSatId() const;
 
-    const rawgpsutils::ScalarT &getTimestamp() const
-    {
-        return timestamp_;
-    }
+    const rawgpsutils::ScalarT & getTimestamp() const;
 
-    rawgpsutils::ScalarT getPseudorange() const
-    {
-        return pseudorange_;
-    }
+    rawgpsutils::ScalarT getPseudorange() const;
 
-    Eigen::Vector3s getSatPosition() const
-    {
-        return sat_position_;
-    }
+    Eigen::Vector3s getSatPosition() const;
 
-    //TODO nel caso voglia calcolarla da fuori. anche se a sto punto devo valutare se tenerla qui o no
-    void setSatPosition(const Eigen::Vector3s &sat_position_)
-    {
-        ObsData::sat_position_ = sat_position_;
-    }
+    void setSatPosition(const Eigen::Vector3s &sat_position_);
 
 protected:
     std::string sat_id_;
